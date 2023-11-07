c374196f7c966cb515e4c8a1bafcc336fb4bfaa1
Hernán Casanova <hernan.casanova.silva@outlook.com>
2023-11-07 13:33:59 -0300

    inicio del proyecto

---
+++ src/test/java/com/pgp/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgp.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
